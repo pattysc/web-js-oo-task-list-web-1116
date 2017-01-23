39e528b64e8385847ccc256357ce9a0c8c17e112
pattysc <pattys@users.noreply.github.com>
2017-01-23 18:07:01 -0500

    meerrrr

--- js/models/list.js
+++ js/models/list.js
@@ -1,3 +1,24 @@
 'use strict';
 // List Model
 
+function List(title) {
+  this.title = title
+  this.id = List.all.length
+  this.tasks = []
+  List.all.push(this)
+}
+
+List.prototype.listEl = function() {
+  return `<div class="list"><h2><button class="destroy-list">x</button> ${this.title}</h2><ul id="list-${this.id}" data-id="${this.id}"></ul></div>`
+}
+
+List.prototype.optionEl = function() {
+  return `<option value="${this.id}">${this.title}</option>`
+}
+
+List.prototype.build = function() {
+  $('#select_list').append(this.listEl())
+  $('#lists').append(this.optionEl())
+}
+
+List.all = []
